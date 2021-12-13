c535514fbc8cd141fb21254987a6dcc4f5e0f4c6
Max Frigge <max@maxfrigge.com>
2021-12-13 16:41:41 +0100

    Add app settings js file

---
+++ assets/app_wishlist-king_settings.js
@@ -0,0 +1,10 @@
+// DO NOT EDIT! This file will be overwritten
+export default {
+  moveToCart: false,
+  loginRequired: false,
+  sharedWishlistPageHandle: "shared-wishlist",
+  wishlistPageHandle: "wishlist",
+  klaviyo: false,
+  facebookPixel: false,
+  googleAnalytics: false,
+}
